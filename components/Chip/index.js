957ce6d06d88d5e0cdf9faf9d43b1d1bd9eac5f1
Chappydev <danjtchapman@gmail.com>
2023-03-25 14:00:35 -0600

    Add onClick prop

--- components/Chip/index.js
+++ components/Chip/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import s from "./Chip.module.scss";
 
-const Chip = ({ children }) => {
-  return <div className={s.wrapper}>{children}</div>;
+const Chip = ({ onClick = function () {}, children }) => {
+  return (
+    <div className={s.wrapper} onClick={onClick}>
+      {children}
+    </div>
+  );
 };
 
 export default Chip;
