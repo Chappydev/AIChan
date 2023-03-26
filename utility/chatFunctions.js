f81a3e14832cd0ea963deb8d437e689e15b1226d
Chappydev <danjtchapman@gmail.com>
2023-03-25 23:47:48 -0600

    Add function for getting responses from chatgpt

---
+++ utility/chatFunctions.js
@@ -0,0 +1,27 @@
+export const getChatResponse = async (keyArr) => {
+  const [url, content] = keyArr;
+  console.log(`url: ${url}, content: ${content}`);
+  const body = JSON.stringify({
+    content: content,
+  });
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const data = await response.json();
+      const error = (data && data.message) || response.status;
+      throw new Error(error);
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+};
