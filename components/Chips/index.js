e7bd30943d58601949a841905f1ea883d3797f12
Chappydev <danjtchapman@gmail.com>
2023-03-29 21:17:36 -0600

    Fix chips to show all options

--- components/Chips/index.js
+++ components/Chips/index.js
@@ -7,7 +7,7 @@ const Chips = ({ options }) => {
   // replace this with some logic to determine how much space there is
   const numberOfChips = 3;
   const visibleOptions = options.slice(0, numberOfChips - 1);
-  const hiddenOptions = options.slice(numberOfChips);
+  const hiddenOptions = options.slice(numberOfChips - 1);
 
   return (
     <div className={s.chips}>
