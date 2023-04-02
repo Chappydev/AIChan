ca5d63c5f68037d75939cce42a818cddced38b4f
Chappydev <danjtchapman@gmail.com>
2023-04-01 21:05:53 -0600

    Add custom endpoint

---
+++ pages/api/custom.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  if (!(req.body && req.body.content)) {
+    return res.status(400).json({ error: "must include content" });
+  }
+  const { content } = req.body;
+
+  try {
+    const { data } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: content,
+      temperature: 0.5,
+    });
+    console.log(data);
+
+    res.status(200).json(data.choices[data.choices.length - 1].message);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+}
