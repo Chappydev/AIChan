128ae345e7b52e4067c9335adf324c4a68c9ca02
Chappydev <danjtchapman@gmail.com>
2023-04-08 15:02:08 -0600

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
