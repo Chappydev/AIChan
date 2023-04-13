c41710de309365565633e0ff32a75adad52804e0
Chappydev <danjtchapman@gmail.com>
2023-04-13 17:44:58 -0600

    Update title

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Japanese Assistant</title>
+        <title>AIChan - Demo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Tour
