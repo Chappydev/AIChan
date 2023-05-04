e09b70eae8283b93781e1f2be287bac4063e0159
Chappydev <danjtchapman@gmail.com>
2023-05-03 23:30:22 -0600

    Add icons for fullscreen

---
+++ components/material-icons/FullscreenExit/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FullscreenExit = ({ size = 36, fill = "white" }) => {
+  return <svg xmlns="http://www.w3.org/2000/svg" height={size} fill={fill} viewBox="0 96 960 960"><path d="M333 856V723H200v-60h193v193h-60Zm234 0V663h193v60H627v133h-60ZM200 489v-60h133V296h60v193H200Zm367 0V296h60v133h133v60H567Z" /></svg>;
+}
+
+export default FullscreenExit;
