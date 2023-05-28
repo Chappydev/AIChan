72ec9f875ecd012011b5df22a6a8412f3e15993d
Chappydev <danjtchapman@gmail.com>
2023-05-27 22:13:32 -0600

    Add human-readable time to the player

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -4,12 +4,26 @@ import useVideo from "@/hooks/useVideo";
 import { Forward5Rounded, FullscreenExitRounded, FullscreenRounded, PauseRounded, PlayArrowRounded } from "@mui/icons-material";
 import SubtitlePlayer from "../SubtitlePlayer";
 import SubtitleManager from "@/utility/SubtitleManager";
+import { getReadableTime } from "@/utility/videoFunctions";
 
-const VideoPlayer = ({ src, subtitles, onTimeUpdate, videoTagRef }) => {
+const VideoPlayer = ({ src, subtitles, videoTagRef }) => {
   const { videoClock, isPlay, isFullscreen, togglePlay, toggleFullScreenMode, skipForwardFive, videoRef, videoContainerRef } = useVideo();
 
   const [currentSubtitles, setCurrentSubtitles] = useState([]);
+  const [currentTime, setCurrentTime] = useState('00:00');
+  const [videoLength, setVideoLength] = useState('00:00');
   const subtitleManager = useMemo(() => new SubtitleManager(subtitles), [subtitles, SubtitleManager]);
+
+  const timeUpdateHandler = (e) => {
+    setCurrentTime(getReadableTime(e.target.currentTime));
+  }
+
+  const durationChangeHandler = (e) => {
+    if (!e.target.duration) {
+      setVideoLength('00:00');
+    }
+    setVideoLength(getReadableTime(e.target.duration));
+  }
 
   useEffect(() => {
     let interval;
@@ -28,15 +42,21 @@ const VideoPlayer = ({ src, subtitles, onTimeUpdate, videoTagRef }) => {
 
   return (
     <div className={s.videoContainer} ref={videoContainerRef}>
-      <video className={s.video} ref={(el) => { videoTagRef.current = el; videoRef.current = el; }} src={src} onTimeUpdate={onTimeUpdate}>
+      <video className={s.video} ref={(el) => { videoTagRef.current = el; videoRef.current = el; }} src={src} onTimeUpdate={timeUpdateHandler} onDurationChange={durationChangeHandler}>
       </video>
       {/* controls */}
       <SubtitlePlayer subtitles={currentSubtitles} />
       <div className={s.controls
       } >
-        <button className={s.playPauseBtn} onClick={togglePlay}>{isPlay ? <PauseRounded fontSize="large" /> : <PlayArrowRounded fontSize="large" />}</button>
-        <button className={s.forwardFiveBtn} onClick={skipForwardFive}>{isPlay ? <Forward5Rounded fontSize="large" /> : <Forward5Rounded fontSize="large" />}</button>
-        <button className={s.fullscreenBtn} onClick={toggleFullScreenMode}>{isFullscreen ? <FullscreenExitRounded fontSize="large" /> : <FullscreenRounded fontSize="large" />}</button>
+        <div className={s.timeControls}>
+          <div>{currentTime}</div>
+          <div>{videoLength}</div>
+        </div>
+        <div className={s.buttonControls}>
+          <button className={s.playPauseBtn} onClick={togglePlay}>{isPlay ? <PauseRounded fontSize="large" /> : <PlayArrowRounded fontSize="large" />}</button>
+          <button className={s.forwardFiveBtn} onClick={skipForwardFive}>{isPlay ? <Forward5Rounded fontSize="large" /> : <Forward5Rounded fontSize="large" />}</button>
+          <button className={s.fullscreenBtn} onClick={toggleFullScreenMode}>{isFullscreen ? <FullscreenExitRounded fontSize="large" /> : <FullscreenRounded fontSize="large" />}</button>
+        </div>
       </div>
     </div >
   )
