c67b291bf150b1c4e0e7d22b254d24afe830464d
Chappydev <danjtchapman@gmail.com>
2023-05-28 21:27:37 -0600

    Remove console logs

--- utility/videoFunctions.js
+++ utility/videoFunctions.js
@@ -1,11 +1,9 @@
 export const getReadableTime = (time) => {
-  console.log('time: ', time);
   const roundedTime = Math.floor(time);
   const seconds = roundedTime % 60;
   const totalMinutes = Math.floor(((roundedTime - seconds) / 60));
   const minutes = totalMinutes % 60;
   const totalHours = Math.floor(((totalMinutes - minutes) / 60));
-  console.log('hours, minutes, secs: ', totalHours, minutes, seconds);
 
   const paddedMinutes = minutes.toString().padStart(2, '0');
   const paddedSeconds = seconds.toString().padStart(2, '0');
