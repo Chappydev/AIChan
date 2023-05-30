8d4315dff99e9132cd4a09fa370ad1fd23fd76ef
Chappydev <danjtchapman@gmail.com>
2023-05-29 23:31:16 -0600

    Display controls only on hover; Improve timeline styles

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -14,6 +14,7 @@ const VideoPlayer = ({ src, subtitles, videoTagRef }) => {
   const [currentTime, setCurrentTime] = useState(0);
   const [videoLength, setVideoLength] = useState(0);
   const [mouseIsDown, setMouseIsDown] = useState(false);
+  const [showControls, setShowControls] = useState(false);
   const wasPaused = useRef(true);
   const subtitleManager = useMemo(() => new SubtitleManager(subtitles), [subtitles, SubtitleManager]);
 
@@ -76,22 +77,28 @@ const VideoPlayer = ({ src, subtitles, videoTagRef }) => {
   // TODO: Change how we manage the width of the 'currentTimeLine' as it does not keep up with dragging and stuff very well
   return (
     <div className={s.videoContainer} ref={videoContainerRef}>
-      <video className={s.video} ref={(el) => { videoTagRef.current = el; videoRef.current = el; }} src={src} onTimeUpdate={timeUpdateHandler} onDurationChange={durationChangeHandler}>
+      <video className={s.video} ref={(el) => { videoTagRef.current = el; videoRef.current = el; }} src={src} onTimeUpdate={timeUpdateHandler} onDurationChange={durationChangeHandler} >
       </video>
       {/* controls */}
       <SubtitlePlayer subtitles={currentSubtitles} />
-      <div className={s.controls
-      } >
-        <div className={s.timeControls}>
-          <div>{getReadableTime(currentTime)}</div>
-          <div className={s.timeLine} onMouseMove={mouseMoveHandler} onMouseUp={mouseUpHandler} onMouseDown={mouseDownHandler}><div className={s.currentTimeLine} style={{ width: `${Math.min(Math.max((currentTime / videoLength) * 100, 0), 100)}%` }}></div></div>
-          <div>{getReadableTime(videoLength)}</div>
-        </div>
-        <div className={s.buttonControls}>
-          <button className={s.playPauseBtn} onClick={togglePlay}>{isPlay ? <PauseRounded fontSize="large" /> : <PlayArrowRounded fontSize="large" />}</button>
-          <button className={s.forwardFiveBtn} onClick={skipForwardFive}>{isPlay ? <Forward5Rounded fontSize="large" /> : <Forward5Rounded fontSize="large" />}</button>
-          <button className={s.fullscreenBtn} onClick={toggleFullScreenMode}>{isFullscreen ? <FullscreenExitRounded fontSize="large" /> : <FullscreenRounded fontSize="large" />}</button>
-        </div>
+      <div className={s.controlsOuterContainer
+      } onMouseEnter={() => setShowControls(true)} onMouseLeave={() => setShowControls(false)}>
+        {showControls &&
+          <div className={s.controlsInnerContainer}>
+            <div className={s.timeControls}>
+              <div className={s.textTime}>{getReadableTime(currentTime)}</div>
+              <div className={s.timeLineContainer}>
+                <div className={s.timeLine} onMouseMove={mouseMoveHandler} onMouseUp={mouseUpHandler} onMouseDown={mouseDownHandler}><div className={s.currentTimeLine} style={{ width: `${Math.min(Math.max((currentTime / videoLength) * 100, 0), 100)}%` }}></div></div>
+              </div>
+              <div className={s.textTime}>{getReadableTime(videoLength)}</div>
+            </div>
+            <div className={s.buttonControls}>
+              <button className={s.playPauseBtn} onClick={togglePlay}>{isPlay ? <PauseRounded fontSize="large" /> : <PlayArrowRounded fontSize="large" />}</button>
+              <button className={s.forwardFiveBtn} onClick={skipForwardFive}>{isPlay ? <Forward5Rounded fontSize="large" /> : <Forward5Rounded fontSize="large" />}</button>
+              <button className={s.fullscreenBtn} onClick={toggleFullScreenMode}>{isFullscreen ? <FullscreenExitRounded fontSize="large" /> : <FullscreenRounded fontSize="large" />}</button>
+            </div>
+          </div>
+        }
       </div>
     </div >
   )
