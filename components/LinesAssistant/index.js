3be2300c6c34b8d634ee82cbb6175617e53c7766
Chappydev <danjtchapman@gmail.com>
2023-05-31 20:23:38 -0600

    Add tabs for lines/chat

--- components/LinesAssistant/index.js
+++ components/LinesAssistant/index.js
@@ -4,62 +4,30 @@ import useChat from "@/hooks/useChat";
 import useOptions from "@/hooks/useOptions";
 import { LineContext } from "@/contexts/LineContext";
 import TextLinesView from "../TextLinesView";
-import ChatBubble from "../ChatBubble";
-import TextArea from "../TextArea";
-import ChatLoadingBubble from "../ChatLoadingBubble";
-import ChatInput from "../ChatInput";
 import s from './LinesAssistant.module.scss';
+import { AssistantChat } from "../AssistantChat";
 
-export const LinesAssistant = ({ lines, shown }) => {
+export const LinesAssistant = ({ lines, currentLines, shown }) => {
   const [refLines, setRefLines] = useState([]);
   const { chat, setChat, ref: scrollRef, onComplete } = useChat(refLines);
   const options = useOptions(refLines, chat, setChat);
   const { isLoading, error } = useAssistant(chat, setChat);
 
+  const [activeTab, setActiveTab] = useState('lines');
+
   return (
     <div className={s.textAndChatWrapper} style={{ flexGrow: shown ? 1 : 0 }}>
+      <div className={s.tabs}>
+        <div onClick={() => setActiveTab('lines')} data-active={(activeTab === "lines").toString()}>Lines</div>
+        <div onClick={() => setActiveTab('chat')} data-active={(activeTab === "chat").toString()}>Chat</div>
+      </div>
       <LineContext.Provider value={{ refLines, setRefLines }}>
-        <TextLinesView lines={lines} />
-        <div className={s.chatWrapper}>
-          <div className={s.chatBubbles} ref={scrollRef}>
-            {chat.map((message, ind) => {
-              if (message.type === "initial") {
-                return (
-                  <ChatBubble
-                    key={ind}
-                    initial={true}
-                    role={message.role}
-                    options={options}
-                  >
-                    {message.content}
-                  </ChatBubble>
-                );
-              } else if (message.waiting) {
-                return (
-                  <ChatBubble key={ind} role={message.role}>
-                    <TextArea
-                      content={message.content}
-                      onComplete={onComplete}
-                    />
-                  </ChatBubble>
-                );
-              }
-
-              return (
-                <ChatBubble key={ind} role={message.role} options={options}>
-                  {message.content}
-                </ChatBubble>
-              );
-            })}
-            {isLoading && <ChatLoadingBubble />}
-            {error && (
-              <ChatBubble error={true} role="assistant">
-                Something went wrong. You may have reached the request
-                limit. Please try again later.
-              </ChatBubble>
-            )}
-          </div>
-          <ChatInput chat={chat} setChat={setChat} />
+        <div className={s.content}>
+          {
+            activeTab === 'lines'
+              ? <TextLinesView lines={lines} currentLines={currentLines} />
+              : <AssistantChat options={options} chat={chat} setChat={setChat} scrollRef={scrollRef} onComplete={onComplete} isLoading={isLoading} error={error} />
+          }
         </div>
       </LineContext.Provider>
     </div>
