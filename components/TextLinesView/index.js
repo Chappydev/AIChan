4635143014544f92e5fdf2078cc9de8cb28dd481
Chappydev <danjtchapman@gmail.com>
2023-05-31 20:24:55 -0600

    Remove unneeded leftovers from demo page

--- components/TextLinesView/index.js
+++ components/TextLinesView/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import s from './TextLinesView.module.scss';
 import TextLine from '../TextLine';
 
-const TextLinesView = ({ lines }) => {
+const TextLinesView = ({ lines, currentLines }) => {
   return (
     <div id="textArea" className={s.textWrapper}>
       <div className={s.textLines}>
-        {lines.map((line, ind) =>
-          line.startsWith("オスタニア") ? (
-            <TextLine id="lineSelector" text={line} key={ind} />
-          ) : (
-            <TextLine text={line} key={ind} />
-          )
-        )}
+        {lines.map((line, ind) => <TextLine text={line.text} key={ind} />)}
       </div>
     </div>
   )
