eb35e93a17b293488db0dea9edd82bf0f5c75207
Chappydev <danjtchapman@gmail.com>
2023-06-13 18:27:36 -0600

    Add Noto Sans JP font

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
 import "@/styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { Noto_Sans_JP } from 'next/font/google';
+
+// const noto = Noto_Sans({
+//   weight: ['300', '500', '700'],
+//   style: ['normal'],
+//   subsets: ['latin'],
+// });
+//
+const notoJP = Noto_Sans_JP({
+  weight: ['300', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${notoJP.style.fontFamily}
+        }
+      `}</style>
       <Component {...pageProps} />
       <Analytics />
     </>
